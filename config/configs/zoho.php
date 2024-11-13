5322f7fc2d88b896f3e16897c93927e3e8fc0d9f
Artem Andrienko <109165450+6metra3@users.noreply.github.com>
2024-11-14 00:15:34 +0200

    .

--- config/configs/zoho.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-        'client_id' => "1000.RN93P6Q8CN7XV20IZSGGMG2QNWA8AG",
-        'client_secret' => "afce85b8ddce1569bdd8f025633dfd6e73803c0835",
-        'refresh_token' => "1000.fc2e57a5938008ad13d9bd725cde4327.9ebd4b5c351ce7f13cce99fd3b80fb51",
-];
